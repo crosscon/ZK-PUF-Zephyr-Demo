2c358c98b5bbb1c3d44c1adc74dbd9bf44834cc6
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-05-23 17:14:10 +0200

    application/src/crosscon_hv: remove hardcoded challenges

--- application/src/crosscon_hv/hv_functions.c
+++ application/src/crosscon_hv/hv_functions.c
@@ -98,10 +98,6 @@ TEE_Result PUF_TA_init(void* shared_mem0,
         if (ret != 0) return TEE_ERROR_GENERIC;
         ret = init_crypto();
         if (ret != 0) return TEE_ERROR_GENERIC;
-        /* For now hardcoding/testing shmem reading */
-        memcpy(shared_mem0, &hardcoded_challenge_0, CHALLENGE_SIZE);
-        memcpy(shared_mem1, &hardcoded_challenge_1, CHALLENGE_SIZE);
-        memcpy(shared_mem2, &hardcoded_challenge_1, NONCE_SIZE);
         has_been_initialized = true;
         return TEE_SUCCESS;
     } else {
--- application/src/puf/puf_prover.h
+++ application/src/puf/puf_prover.h
@@ -14,10 +14,6 @@
 
 #define RESPONSE_SIZE CHALLENGE_SIZE+PUF_KEY_SIZE
 
-// Challenge arrays declaration
-extern const uint8_t hardcoded_challenge_0[CHALLENGE_SIZE];
-extern const uint8_t hardcoded_challenge_1[CHALLENGE_SIZE];
-
 int get_response_to_challenge(uint8_t *challenge, mbedtls_mpi *response);
 int get_commited_value(mbedtls_mpi *response_0, mbedtls_mpi *response_1, mbedtls_ecp_point *commitment);
 int extract_raw_commitment(mbedtls_ecp_point *commitment, uint8_t *raw_commitment);
