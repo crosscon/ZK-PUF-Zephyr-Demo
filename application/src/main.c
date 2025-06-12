4b4bbe55dd5d3fad549b27df6f8f71beeefeae00
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-06-12 17:49:06 +0200

    application/src: Initial TEE Client Driver

--- application/src/main.c
+++ application/src/main.c
@@ -7,106 +7,158 @@ LOG_MODULE_REGISTER(GUEST_VM, LOG_LEVEL_DBG);
 
 #define IS_WRITE_TO_FLASH_ENABLED 1
 
-#define CHALLENGE_SIZE 16
-#define NONCE_SIZE 16
+#define PUF_TA_INIT_FUNC_ID            0x00112233
+#define PUF_TA_GET_COMMITMENT_FUNC_ID  0x11223344
+#define PUF_TA_GET_ZK_PROOFS_FUNC_ID   0x22334455
 
 void vm_init() {
     IRQ_CONNECT(IPC_IRQ_ID, 0, ipc_irq_handler, NULL, 0);
     irq_enable(IPC_IRQ_ID);
-    clear_mem();
     LOG_INF("VM Initialized");
-}
-
-
-void clear_mem(void)
-{
-    memset(message[0], 0, MESSAGE0_SIZE);
-    memset(message[1], 0, MESSAGE1_SIZE);
-    memset(message[2], 0, MESSAGE2_SIZE);
-    memset(message[3], 0, MESSAGE3_SIZE);
-    memset(message[4], 0, MESSAGE4_SIZE);
-    memset(message[5], 0, MESSAGE5_SIZE);
-    memset(message[6], 0, MESSAGE6_SIZE);
-    memset(message[7], 0, MESSAGE7_SIZE);
-    memset(message[8], 0, MESSAGE8_SIZE);
-    memset(message[9], 0, MESSAGE9_SIZE);
-    memset(message[10], 0, MESSAGE10_SIZE);
-    memset(message[11], 0, MESSAGE11_SIZE);
-    memset(message[12], 0, MESSAGE12_SIZE);
-    memset(message[13], 0, MESSAGE13_SIZE);
 }
 
 int main(void)
 {
-    static const uint8_t uuid0[TEE_UUID_LEN] = {
-        0x00, 0x11, 0x22, 0x33,   /* timeLow    */
-        0x44, 0x55,               /* timeMid    */
-        0x66, 0x77,               /* timeHi+ver */
-        0x88, 0x99, 0xAA, 0xBB,   /* clockSeq   */
-        0xCC, 0xDD, 0xEE, 0xFF    /* node       */
-    };
-
-    static const uint8_t uuid1[TEE_UUID_LEN] = {
-        0x11, 0x22, 0x33, 0x44,   /* timeLow    */
-        0x55, 0x66,               /* timeMid    */
-        0x77, 0x88,               /* timeHi+ver */
-        0x99, 0xAA, 0xBB, 0xCC,   /* clockSeq   */
-        0xDD, 0xEE, 0xFF, 0x00    /* node       */
-    };
-
-    static const uint8_t uuid2[TEE_UUID_LEN] = {
-        0x22, 0x33, 0x44, 0x55,   /* timeLow    */
-        0x66, 0x77,               /* timeMid    */
-        0x88, 0x99,               /* timeHi+ver */
-        0xAA, 0xBB, 0xCC, 0xDD,   /* clockSeq   */
-        0xEE, 0xFF, 0x00, 0x11    /* node       */
-    };
-
-    static const uint8_t challenge_1[CHALLENGE_SIZE] = {
-        0x3C, 0xA1, 0xF4, 0x92,
-        0x57, 0xB8, 0x0E, 0x6D,
-        0x1F, 0xA9, 0xC3, 0xE7,
-        0x74, 0x90, 0x12, 0xAD
-    };
-
-    static const uint8_t challenge_2[CHALLENGE_SIZE] = {
-        0xFF, 0x00, 0xFF, 0x00,
-        0xFF, 0x00, 0xFF, 0x00,
-        0x00, 0xFF, 0x00, 0xFF,
-        0x00, 0xFF, 0x00, 0xFF
-    };
-
-    static const uint8_t nonce[NONCE_SIZE] = {
-        0xAA, 0xBB, 0xCC, 0xDD,
-        0xEE, 0xFF, 0xAA, 0xBB,
-        0xCC, 0xDD, 0xEE, 0xFF,
-        0x00, 0x11, 0x22, 0x33
-    };
+    /* Hardcoded session */
+    int session_id = 0;
 
     vm_init();
 
-    LOG_INF("Calling func 1");
-    memcpy((void*)message[0], &uuid0, sizeof(uuid0));
-    ipc_notify(0,0);
+    const uint8_t challenge1[32] = {
+        0xD1, 0x33, 0x53, 0xE8, 0x6B, 0x41, 0xF9, 0x4C,
+        0x88, 0x77, 0xF6, 0x8F, 0xB9, 0x5A, 0xAD, 0x0A,
+        0x35, 0x82, 0x06, 0x95, 0xE2, 0x03, 0x74, 0x13,
+        0xBD, 0x57, 0xA9, 0xC4, 0x47, 0xDF, 0x11, 0xD9
+    };
+
+    const uint8_t challenge2[32] = {
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77
+    };
+
+    const uint8_t nonce[64] = {
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+        0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77
+    };
+
+    LOG_INF("VM Initialized");
+    const struct device *tee_dev = device_get_binding("crosscon_hv_tee");
+    if (tee_dev == NULL) {
+        LOG_ERR("Failed to bind device 'crosscon_hv_tee'");
+        return -1;
+    }
+    struct tee_version_info info;
+    int res;
+    res = tee_get_version(tee_dev, &info);
+    if (res == 0) {
+        LOG_INF("TEE version info:");
+        LOG_INF("impl_id   = %u", info.impl_id);
+        LOG_INF("impl_caps = 0x%08x", info.impl_caps);
+        LOG_INF("gen_caps  = 0x%08x", info.gen_caps);
+    } else {
+        LOG_ERR("tee_get_version() failed: %d", res);
+    }
+
+    ipc_shm.dev   = tee_dev;                            // from device_get_binding()
+    ipc_shm.addr  = (void *)(uintptr_t)VMS_PAYLOAD_PTR; // static shared memory base
+    ipc_shm.size  = (uint64_t)VMS_IPC_PAYLOAD_SIZE;     // total available size
+    ipc_shm.flags = 0;                                  // no special flags
+
+    /* Buffer to write input to */
+    volatile uint8_t *shm_ptr = (volatile uint8_t *)ipc_shm.addr;
+
+    LOG_INF("TEE Initialized");
+
+    param[0].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[0].a    = (uint64_t)(VMS_MEMREF0_OFFSET); // offsets
+    param[0].b    = (uint64_t)32;                   // length: 32 bytes
+    param[0].c    = (uint64_t)(uintptr_t)&ipc_shm;  // shared-memory ID (handle)
+
+    param[1].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[1].a    = (uint64_t)(VMS_MEMREF1_OFFSET); // offsets
+    param[1].b    = (uint64_t)32;                   // length: 32 bytes
+    param[1].c    = (uint64_t)(uintptr_t)&ipc_shm;  // shared-memory ID (handle)
+
+    param[2].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[2].a    = (uint64_t)(VMS_MEMREF2_OFFSET); // offsets
+    param[2].b    = (uint64_t)32;                   // length: 32 bytes
+    param[2].c    = (uint64_t)(uintptr_t)&ipc_shm;  // shared-memory ID (handle)
+
+    param[3].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[3].a    = (uint64_t)(VMS_MEMREF3_OFFSET); // offsets
+    param[3].b    = (uint64_t)32;                   // length: 32 bytes
+    param[3].c    = (uint64_t)(uintptr_t)&ipc_shm;  // shared-memory ID (handle)
+
+    /* PUF_TA_init */
+    arg.func      = PUF_TA_INIT_FUNC_ID;
+    arg.session   = session_id;
+    arg.cancel_id = 0;
+    arg.ret       = 0;
+    arg.ret_origin= 0;
+
+    res = tee_invoke_func(tee_dev, &arg, 4, &param);
+
+    LOG_INF("PUF_TA_INIT called");
 
     k_msleep(500);
-    clear_mem();
 
-    LOG_INF("Calling func 2");
-    memcpy((void*)message[0], &uuid1, sizeof(uuid1));
-    memcpy((void*)message[2], &challenge_1, sizeof(challenge_1));
-    memcpy((void*)message[3], &challenge_2, sizeof(challenge_2));
-    ipc_notify(0,0);
+    /* PUF_TA_get_commitment */
+    arg.func = PUF_TA_GET_COMMITMENT_FUNC_ID;
+
+    param[0].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INPUT;
+    param[1].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INPUT;
+    param[2].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[3].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+
+    memset(shm_ptr, 0, 1024);
+    memcpy((void *)(shm_ptr + param[0].a), challenge1, param[0].b);
+    memcpy((void *)(shm_ptr + param[1].a), challenge2, param[1].b);
+
+    res = tee_invoke_func(tee_dev, &arg, 4, &param);
+
+    LOG_INF("PUF_TA_GET_COMMITMENT called");
 
     k_msleep(500);
-    clear_mem();
 
-    LOG_INF("Calling func 3");
-    memcpy((void*)message[0], &uuid2, sizeof(uuid2));
-    memcpy((void*)message[2], &challenge_1, sizeof(challenge_1));
-    memcpy((void*)message[3], &challenge_2, sizeof(challenge_2));
-    memcpy((void*)message[4], &nonce, sizeof(nonce));
-    ipc_notify(0,0);
+    /* PUF_TA_get_zk_proofs_func_id */
+    arg.func = PUF_TA_GET_ZK_PROOFS_FUNC_ID;
+
+    param[0].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INOUT;
+    param[0].a    = (uint64_t)(VMS_MEMREF0_OFFSET);
+    param[0].b    = (uint64_t)32;
+    param[0].c    = (uint64_t)(uintptr_t)&ipc_shm;
+
+    param[1].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INOUT;
+    param[1].a    = (uint64_t)(VMS_MEMREF1_OFFSET);
+    param[1].b    = (uint64_t)32;
+    param[1].c    = (uint64_t)(uintptr_t)&ipc_shm;
+
+    param[2].attr = TEE_PARAM_ATTR_TYPE_MEMREF_INOUT;
+    param[2].a    = (uint64_t)(VMS_MEMREF2_OFFSET);
+    param[2].b    = (uint64_t)64;
+    param[2].c    = (uint64_t)(uintptr_t)&ipc_shm;
+
+    param[3].attr = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+    param[3].a    = (uint64_t)(VMS_MEMREF3_OFFSET);
+    param[3].b    = (uint64_t)64;
+    param[3].c    = (uint64_t)(uintptr_t)&ipc_shm;
+
+    memset(shm_ptr, 0, 1024);
+    memcpy((void *)(shm_ptr + param[0].a), challenge1, param[0].b);
+    memcpy((void *)(shm_ptr + param[1].a), challenge2, param[1].b);
+    memcpy((void *)(shm_ptr + param[2].a), nonce, param[2].b);
+
+    res = tee_invoke_func(tee_dev, &arg, 4, &param);
+
+    LOG_INF("PUF_TA_GET_ZK_PROOFS called");
 
     // Wait for interrupts and handle them according to function_table
     while(1);
