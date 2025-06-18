1005fe54c3557a758e14de1befddf8233fda359d
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-06-18 13:10:31 +0200

    application/src/crosscon_hv/crosscon_hv_config.h: add session handling vars

--- application/src/crosscon_hv/crosscon_hv_config.h
+++ application/src/crosscon_hv/crosscon_hv_config.h
@@ -12,41 +12,50 @@
 // Base Config
 // ----------------------------------------
 
-#define IPC_IRQ_ID                 63     //79 on Bao config
-#define CROSSCON_HV_IMAGE_START    0x10000000UL
-#define CROSSCON_HV_HC_OFF         0x41UL
-#define CROSSCON_HV_HC_ADDR        ((uintptr_t)CROSSCON_HV_IMAGE_START + CROSSCON_HV_HC_OFF)
-#define CROSSCON_HV_HC_IPC_ID      0x1
+#define IPC_IRQ_ID                      63     //79 on Bao config
+#define CROSSCON_HV_IMAGE_START         0x10000000UL
+#define CROSSCON_HV_HC_OFF              0x41UL
+#define CROSSCON_HV_HC_ADDR             ((uintptr_t)CROSSCON_HV_IMAGE_START + CROSSCON_HV_HC_OFF)
+#define CROSSCON_HV_HC_IPC_ID           0x1
 
-#define VMS_IPC_BASE               0x20017000UL
-#define VMS_MAX_PARAMS             4
-#define VMS_IPC_PAYLOAD_SIZE       1024UL
-#define VMS_MEMREF_SLOT_SIZE       (VMS_IPC_PAYLOAD_SIZE / VMS_MAX_PARAMS)
-#define VMS_TOTAL_SIZE             (VMS_ARG_SIZE + VMS_PARAM_SIZE + VMS_IPC_PAYLOAD_SIZE)
+#define VMS_IPC_BASE                    0x20017000UL
+#define VMS_MAX_PARAMS                  4
+#define VMS_IPC_PAYLOAD_SIZE            1024UL
+#define VMS_MEMREF_SLOT_SIZE            (VMS_IPC_PAYLOAD_SIZE / VMS_MAX_PARAMS)
 
 // ----------------------------------------
 // Struct sizes
 // ----------------------------------------
 
-#define VMS_PARAM_SIZE             (VMS_MAX_PARAMS * sizeof(GP_Param))
-#define VMS_ARG_SIZE               sizeof(GP_InvokeArgs)
+#define VMS_CALL_TYPE_SIZE              sizeof(tee_call_type_t)
+#define VMS_SESSION_ARGS_SIZE           sizeof(GP_OpenSessionArgs)
+#define VMS_INVOKE_FUNC_ARGS_SIZE       sizeof(GP_InvokeArgs)
+#define VMS_PARAM_SIZE                  (VMS_MAX_PARAMS * sizeof(GP_Param))
 
 // ----------------------------------------
 // Offsets into Shared Memory
 // ----------------------------------------
 
-#define VMS_OFFSET_INVOKE_ARGS     0x0000
-#define VMS_OFFSET_PARAMS          (VMS_OFFSET_INVOKE_ARGS + VMS_ARG_SIZE)
-#define VMS_OFFSET_PAYLOAD         (VMS_OFFSET_PARAMS + VMS_PARAM_SIZE)
+#define VMS_OFFSET_CALL_TYPE            0x0000
+#define VMS_OFFSET_SESSION_ARGS         (VMS_OFFSET_CALL_TYPE + VMS_CALL_TYPE_SIZE)
+#define VMS_OFFSET_INVOKE_FUNC_ARGS     (VMS_OFFSET_SESSION_ARGS + VMS_SESSION_ARGS_SIZE)
+#define VMS_OFFSET_PARAMS               (VMS_OFFSET_INVOKE_FUNC_ARGS + VMS_INVOKE_FUNC_ARGS_SIZE)
+#define VMS_OFFSET_PAYLOAD              (VMS_OFFSET_PARAMS + VMS_PARAM_SIZE)
 
-#define VMS_MEMREF0_OFFSET         0
-#define VMS_MEMREF1_OFFSET         (1 * VMS_MEMREF_SLOT_SIZE)
-#define VMS_MEMREF2_OFFSET         (2 * VMS_MEMREF_SLOT_SIZE)
-#define VMS_MEMREF3_OFFSET         (3 * VMS_MEMREF_SLOT_SIZE)
+#define VMS_MEMREF0_OFFSET              0
+#define VMS_MEMREF1_OFFSET              (1 * VMS_MEMREF_SLOT_SIZE)
+#define VMS_MEMREF2_OFFSET              (2 * VMS_MEMREF_SLOT_SIZE)
+#define VMS_MEMREF3_OFFSET              (3 * VMS_MEMREF_SLOT_SIZE)
 
 // ----------------------------------------
 // Packed Structs
 // ----------------------------------------
+
+typedef enum {
+    TEE_CALL_TYPE_INVOKE_FUNC,
+    TEE_CALL_TYPE_OPEN_SESSION,
+    TEE_CALL_TYPE_CLOSE_SESSION,
+} tee_call_type_t;
 
 typedef struct __packed {
     uint32_t func;
@@ -65,9 +74,22 @@ typedef struct __packed {
 } GP_Param;
 
 typedef struct __packed {
-    GP_InvokeArgs invoke_args;
-    GP_Param      params[VMS_MAX_PARAMS];
-    uint8_t       payload[VMS_IPC_PAYLOAD_SIZE];
+    uint8_t  uuid[TEE_UUID_LEN];
+    uint8_t  clnt_uuid[TEE_UUID_LEN];
+    uint32_t clnt_login;
+    uint32_t cancel_id;
+    uint32_t session;
+    uint32_t ret;
+    uint32_t ret_origin;
+    uint32_t paramTypes;
+} GP_OpenSessionArgs;
+
+typedef struct __packed {
+    tee_call_type_t    call_type;
+    GP_OpenSessionArgs session_args;
+    GP_InvokeArgs      invoke_args;
+    GP_Param           params[VMS_MAX_PARAMS];
+    uint8_t            payload[VMS_IPC_PAYLOAD_SIZE];
 } GP_SharedMessage;
 
 /* Macro to encode paramTypes field */
@@ -81,12 +103,14 @@ typedef struct __packed {
 // Direct Access Pointers
 // ----------------------------------------
 
-#define VMS_HEADER_PTR             ((volatile uint8_t *)VMS_IPC_BASE)
-#define VMS_PAYLOAD_PTR            ((volatile uint8_t *)(VMS_IPC_BASE + VMS_OFFSET_PAYLOAD))
-#define GP_SHARED_MSG_PTR          ((volatile GP_SharedMessage *)VMS_IPC_BASE)
+#define VMS_HEADER_PTR                  ((volatile uint8_t *)(VMS_IPC_BASE + VMS_OFFSET_SESSION_ARGS))
+#define VMS_PAYLOAD_PTR                 ((volatile uint8_t *)(VMS_IPC_BASE + VMS_OFFSET_PAYLOAD))
 
-#define GP_ARGS_PTR                ((volatile GP_InvokeArgs *)(VMS_IPC_BASE + VMS_OFFSET_INVOKE_ARGS))
-#define GP_PARAMS_PTR              ((volatile GP_Param *)(VMS_IPC_BASE + VMS_OFFSET_PARAMS))
+#define GP_SHARED_MSG_PTR               ((volatile GP_SharedMessage *)(VMS_IPC_BASE))
+#define GP_CALL_TYPE_PTR                ((volatile tee_call_type_t *)(VMS_IPC_BASE + VMS_OFFSET_CALL_TYPE))
+#define GP_SESSION_ARGS_PTR             ((volatile GP_OpenSessionArgs *)(VMS_IPC_BASE + VMS_OFFSET_SESSION_ARGS))
+#define GP_INVOKE_FUNC_ARGS_PTR         ((volatile GP_InvokeArgs *)(VMS_IPC_BASE + VMS_OFFSET_INVOKE_FUNC_ARGS))
+#define GP_PARAMS_PTR                   ((volatile GP_Param *)(VMS_IPC_BASE + VMS_OFFSET_PARAMS))
 
 // ----------------------------------------
 // Global Declarations
