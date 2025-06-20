e104cd8582730fbfbe4413a1a992afc354e81c18
Muhammad <s473711>
2025-06-20 17:31:29 +0200

    Changed to sys_csrand_get() method instad of rand() inorder to make it non-deterministic

--- application/src/crypto/mbedtls/crypto_handler.c
+++ application/src/crypto/mbedtls/crypto_handler.c
@@ -2,6 +2,7 @@
 LOG_MODULE_DECLARE(PUF_VM);
 
 #include "crypto_handler.h"
+#include <zephyr/random/random.h>
 
 #define MBEDTLS_HEAP_SIZE  8192
 #define MAX_HASH_TRIES 10
@@ -44,25 +45,9 @@ void log_mpi_hex(const char *label, const mbedtls_mpi *X)
 }
 
 int rand_function(void *rng_state, unsigned char *output, size_t len) {
-
-    size_t use_len;
-	int rnd;
-
-	if (rng_state != NULL)
-		rng_state = NULL;
-
-	while (len > 0) {
-		use_len = len;
-		if (use_len > sizeof(int))
-			use_len = sizeof(int);
-
-		rnd = rand();
-		memcpy(output, &rnd, use_len);
-		output += use_len;
-		len -= use_len;
-	}
-
-	return (0);
+    (void)rng_state;
+    sys_csrand_get(output, len);  
+    return 0;
 }
 
 int get_random_mpi(mbedtls_mpi *X)
