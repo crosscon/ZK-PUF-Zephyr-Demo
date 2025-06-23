826b962a08d6c332b26b955344078a1619b89734
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-06-23 10:27:38 +0200

    application: strip PUF logic

--- application/src/main.c
+++ application/src/main.c
@@ -1,11 +1,11 @@
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(PUF_VM, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(GUEST_VM, LOG_LEVEL_DBG);
 
 #include <stdio.h>
 #include "crosscon_hv_config.h"
 
 void vm_init() {
-    IRQ_CONNECT(IPC_IRQ_ID, 0, ipc_irq_ta_handler, NULL, 0);
+    IRQ_CONNECT(IPC_IRQ_ID, 0, ipc_irq_client_handler, NULL, 0);
     irq_enable(IPC_IRQ_ID);
     LOG_INF("VM Initialized");
 }
